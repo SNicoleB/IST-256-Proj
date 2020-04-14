529a35b64f7f610d86c41a47d60980d22c86e26e
jmf6151 <55959290+jmf6151@users.noreply.github.com>
2020-04-14 00:22:50 -0400

    Type functionality and price css

---
+++ IST Group Proj/scripts/byType.js
@@ -0,0 +1,20 @@
+// when user clicks button, toggle between hiding and showing dropdown content //
+function dropDown() {
+  document.getElementById("typeDropdown").classList.toggle("show");
+}
+
+//close dropdown if someone clicks outside of it//
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+// credit for dropdown code to w3schools //
