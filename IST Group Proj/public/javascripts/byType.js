6422490809f86df585d25d85c9e7ac8bc2e779e2
Sarah Bodnar <sarahbodnar@Sarahs-MBP-2.Home>
2020-04-25 21:02:17 -0400

    Added API and display/hide functionality on results for byType

--- IST Group Proj/public/javascripts/byType.js
+++ IST Group Proj/public/javascripts/byType.js
@@ -18,3 +18,32 @@ window.onclick = function(event) {
 }
 
 // credit for dropdown code to w3schools //
+
+$.ajax({
+  type:"get",
+  url:"https://developers.zomato.com/api/v2.1/categories",
+  headers: {
+    'user-key': "7749b19667964b87a3efc739e254ada2",
+  },
+  success: function(data){
+    console.log(data);
+  }
+});
+/* references for how to integrate api key:
+https://stackoverflow.com/questions/45261255/how-to-use-an-api-key-for-an-ajax-call
+https://stackoverflow.com/questions/3258645/pass-request-headers-in-a-jquery-ajax-get-call*/
+
+function displayDResults() {
+  document.getElementById("deliveryResultsID").classList.remove("deliveryResults");
+  document.getElementById("deliveryResultsID").classList.add("show");
+}
+
+/*
+$.ajax({
+  type:"get",
+  url:"https://developers.zomato.com/api/v2.1/categories/delivery",
+  success: function(data){
+      console.log(data.categories[1].categories);
+  }
+});
+*/
